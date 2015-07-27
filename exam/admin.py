98ca5730732928bcc32bc09f0422d33ec4d8540b
alejandro <alex@example.com>
2015-07-27 17:03:45 +0200

    27/07/15

---
+++ exam/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from exam.models import Test, Question, Response
+
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Response)
---
+++ exam/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+class Test(models.Model):
+	name = models.TextField()
+	description = models.TextField()
+	
+	# Getter name
+	def __unicode__(self):
+		return (self.name)
+
+class Question(models.Model):
+	TEXT = 'text'
+	RADIO = 'radio'
+	SELECT = 'select'
+	SELECT_MULTIPLE = 'select-multiple'
+	INTEGER = 'integer'
+
+	QUESTION_TYPES = (
+		(TEXT, 'text'),
+		(RADIO, 'radio'),
+		(SELECT, 'select'),
+		(SELECT_MULTIPLE, 'Select Multiple'),
+		(INTEGER, 'integer'),
+	)
+
+	text = models.TextField()
+	required = models.BooleanField()
+	test = models.ForeignKey(Test)
+	question_type = models.CharField(max_length=200, choices=QUESTION_TYPES, default=TEXT)
+
+	# Getter name
+	def __unicode__(self):
+		return (self.text)
+
+class Response(models.Model):
+	text = models.TextField()
+	isCorrect = models.BooleanField()
+	question = models.ForeignKey(Question)
+
+	# Getter name
+	def __unicode__(self):
+		return (self.text)
