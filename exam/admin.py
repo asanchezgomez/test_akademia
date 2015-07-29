9ce3a81612a135ade3314486d628f27ec8c57d35
alejandro <alex@example.com>
2015-07-29 10:41:05 +0200

    29/07/15 -  Added two level inline in admin console

--- exam/admin.py
+++ exam/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from exam.models import Test, Question, Response
+from nested_inlines.admin import NestedModelAdmin, NestedStackedInline, NestedTabularInline
 
-admin.site.register(Test)
-admin.site.register(Question)
-admin.site.register(Response)
+class ResponseInline(NestedTabularInline):
+	model = Response
+	extra = 0
+
+class QuestionInline(NestedTabularInline):
+	model = Question
+	extra = 0
+	inlines = [ResponseInline,]
+	
+class TestAdmin(NestedModelAdmin):
+	inlines = [QuestionInline,]
+
+admin.site.register(Test, TestAdmin)
